e0f579a92306e3dcc8f83d27b38e0a3b88825250
Jhon deiber arcila castano <57459718+JhonCodeU@users.noreply.github.com>
2022-07-25 22:24:09 +0000

    go connect to postgresDB

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/JhonCodeU/Go-PostgreSQL-REST-API-/db"
 	"github.com/JhonCodeU/Go-PostgreSQL-REST-API-/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	db.DBConnection()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
